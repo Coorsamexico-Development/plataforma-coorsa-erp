c6ad5becc6384d508e5f7f761a58f0e4d3c58e5f
Alan Tonatiuh <programadorsr@coorsamexico.com>
2023-01-12 11:14:46 -0600

    images logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,10 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="https://coorsamexico.com/assets/img/PNG/logos/Logo_Rosa_Coorsa.png" class="logo" alt="Coorsamexico Logo">
-@else
-{{ $slot }}
 @endif
 </a>
 </td>
