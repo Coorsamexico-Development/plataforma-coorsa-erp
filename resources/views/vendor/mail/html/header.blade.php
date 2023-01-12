ca314d9ca44274dd50d19af8cc0fe214aff8cb80
Alan Tonatiuh <programadorsr@coorsamexico.com>
2023-01-12 11:21:55 -0600

    End if

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,6 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 <img src="https://coorsamexico.com/assets/img/PNG/logos/Logo_Rosa_Coorsa.png" class="logo" alt="Coorsamexico Logo">
-@endif
 </a>
 </td>
 </tr>
