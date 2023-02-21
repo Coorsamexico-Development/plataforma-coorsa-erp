6e41a97371172765620f620a849a0d2569bde7e5
Hector <programadorjr@coorsamexico.com>
2023-02-21 09:08:18 -0600

    icons

--- resources/js/Pages/CardViewUser/IndexCard.vue
+++ resources/js/Pages/CardViewUser/IndexCard.vue
@@ -2,11 +2,18 @@
 
 </script>
 <template>
+  <span class="absolute mt-8 ml-8">
+    <svg xmlns="http://www.w3.org/2000/svg" width="15.786" height="25.659" viewBox="0 0 15.786 25.659">
+      <path id="Trazado_3" data-name="Trazado 3" d="M18.949,76.591l-2.671-2.828,10.182-9.62L16.219,53.651,19,50.932l13,13.324Z" transform="translate(-16.219 -50.932)" fill="#e84a49"/>
+    </svg>
+  </span>
+  <span class="absolute h-12 bg-[#E84A49] ml-72" style="width:2px;"></span>
   <div>
      <img src="../../../img/renato.jpg" />
   </div>
   <div class="bg-white w-full h-32"  style="transform: skewY(-11deg); margin-top: -3rem; ">
   </div>
+  <span class="absolute h-14 bg-[#E84A49] ml-8" style="width:2px; margin-top:-8rem"></span>
   <div class="flex flex-col  relative" style="font-family: 'Montserrat'; z-index: 5; margin-top: -6rem;">
     <div class="flex flex-col justify-center items-center">
         <h1 class="uppercase font-semibold" style="letter-spacing: 4px; font-size: 1.5rem;">RENATO ORTIZ</h1>
@@ -40,91 +47,28 @@
   <div class="flex flex-row mt-8 justify-center ">
      <div class="m-2">
         <a href="tel: +52 55 8025 5972">
-            <button class="border rounded-full p-3 bg-[#F7F7F7]">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="25.001" height="25.128" viewBox="0 0 25.001 25.128">
-                  <defs>
-                    <clipPath id="clip-path">
-                      <rect id="Rectángulo_44" data-name="Rectángulo 44" width="25.001" height="25.128" fill="none"/>
-                    </clipPath>
-                  </defs>
-                  <g id="Grupo_105" data-name="Grupo 105" opacity="0.35">
-                    <g id="Grupo_104" data-name="Grupo 104">
-                      <g id="Grupo_103" data-name="Grupo 103" clip-path="url(#clip-path)">
-                        <path id="Trazado_29" data-name="Trazado 29" d="M47.813,361.211c-.352-.057-.705-.106-1.057-.172a15.15,15.15,0,0,1-3.881-1.38,25.035,25.035,0,0,1-5.614-3.727,25.858,25.858,0,0,1-7.589-10.862,9.42,9.42,0,0,1-.573-3.315,4.081,4.081,0,0,1,1.173-2.877c.647-.658,1.3-1.314,1.955-1.96a3.081,3.081,0,0,1,1.576-.835h.636a3.709,3.709,0,0,1,1.924,1.222c.969,1,1.971,1.959,2.915,2.977a2.292,2.292,0,0,1,.009,3.219c-.6.667-1.25,1.288-1.889,1.92a.312.312,0,0,0-.088.431A14.652,14.652,0,0,0,39.9,349.43a17.906,17.906,0,0,0,3.452,2.921c.343.218.713.4,1.065.6a.2.2,0,0,0,.291-.046q.816-.83,1.644-1.649a3.082,3.082,0,0,1,1.12-.773,2.3,2.3,0,0,1,2.523.579c1.094,1.064,2.171,2.149,3.243,3.239a2.419,2.419,0,0,1,.156,3.551c-.737.848-1.548,1.636-2.364,2.413a3.541,3.541,0,0,1-2.078.9,1,1,0,0,0-.161.046Zm.489-1.266a2.681,2.681,0,0,0,2-.711q1.1-1.066,2.136-2.2a1.15,1.15,0,0,0-.022-1.732q-1.649-1.7-3.342-3.348a1.084,1.084,0,0,0-1.493-.077,3.09,3.09,0,0,0-.348.31c-.606.6-1.211,1.215-1.819,1.819a1.042,1.042,0,0,1-1.338.218c-.355-.181-.713-.365-1.054-.57a18.848,18.848,0,0,1-4.117-3.415,15.238,15.238,0,0,1-2.847-4.051,1.13,1.13,0,0,1,.293-1.535c.6-.579,1.2-1.16,1.781-1.755a1.222,1.222,0,0,0,.011-1.981l-.035-.035c-1-1.006-2.006-2.016-3.016-3.016a2.026,2.026,0,0,0-.548-.4,1.193,1.193,0,0,0-1.418.355c-.643.627-1.29,1.253-1.9,1.913a3.466,3.466,0,0,0-.647,1,4.233,4.233,0,0,0-.079,2.51,15.991,15.991,0,0,0,1.781,4.6,26.485,26.485,0,0,0,5.4,6.774,24.187,24.187,0,0,0,7.318,4.6,9.648,9.648,0,0,0,3.3.731" transform="translate(-29.098 -336.083)"/>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
+            <button class="border rounded-full p-3 bg-[#F7F7F7] drop-shadow-xl">
+              <img src="../../../img/telefono.svg" />
             </button>
         </a>
      </div>
      <div class="m-2">
         <a href="https://api.whatsapp.com/send?phone=5580255972">
-            <button class="border rounded-full p-3 bg-[#F7F7F7]">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="26.205" height="26.22" viewBox="0 0 26.205 26.22">
-                  <defs>
-                    <clipPath id="clip-path">
-                      <rect id="Rectángulo_42" data-name="Rectángulo 42" width="26.205" height="26.22" fill="none"/>
-                    </clipPath>
-                    <clipPath id="clip-path-2">
-                      <rect id="Rectángulo_43" data-name="Rectángulo 43" width="17.196" height="16.083" fill="none"/>
-                    </clipPath>
-                  </defs>
-                  <g id="Grupo_128" data-name="Grupo 128" transform="translate(-120 -575)">
-                    <g id="Grupo_99" data-name="Grupo 99" transform="translate(120 575)" opacity="0.35">
-                      <g id="Grupo_98" data-name="Grupo 98">
-                        <g id="Grupo_97" data-name="Grupo 97" clip-path="url(#clip-path)">
-                          <path id="Trazado_27" data-name="Trazado 27" d="M79.18,362c-.548-.075-1.1-.132-1.644-.231a12.366,12.366,0,0,1-4.057-1.488.671.671,0,0,0-.6-.066c-1.568.493-3.14.973-4.711,1.457-.024.009-.049.013-.073.022a.529.529,0,0,1-.6-.093.548.548,0,0,1-.11-.592c.456-1.642.905-3.285,1.365-4.925a.543.543,0,0,0-.059-.476,12.556,12.556,0,0,1-1.731-4.932,12.808,12.808,0,0,1,2.574-9.726,13.1,13.1,0,0,1,23.213,5.211c.121.555.169,1.129.249,1.693.015.09.033.181.048.271v1.535c-.02.185-.042.368-.062.553A13.144,13.144,0,0,1,82.2,361.8c-.493.086-.993.136-1.49.2Zm-10.6-1.523c.106-.028.17-.04.233-.061q1.844-.566,3.688-1.13c.956-.291.685-.313,1.563.172a11.892,11.892,0,0,0,7.7,1.391,11.581,11.581,0,0,0,6.574-3.271A11.82,11.82,0,0,0,91.9,347.1a11.4,11.4,0,0,0-3.252-6.581,11.823,11.823,0,0,0-10.5-3.578,11.457,11.457,0,0,0-6.209,2.9,12.082,12.082,0,0,0-2.274,15.422,1.021,1.021,0,0,1,.123.892c-.41,1.424-.8,2.853-1.209,4.328" transform="translate(-66.848 -335.786)"/>
-                        </g>
-                      </g>
-                    </g>
-                    <g id="Grupo_102" data-name="Grupo 102" transform="translate(124 580)" opacity="0.35">
-                      <g id="Grupo_101" data-name="Grupo 101">
-                        <g id="Grupo_100" data-name="Grupo 100" clip-path="url(#clip-path-2)">
-                          <path id="Trazado_28" data-name="Trazado 28" d="M81.354,354.634a10.875,10.875,0,0,1-6.371-2.358,16.243,16.243,0,0,1-5.044-6.523,5.791,5.791,0,0,1,.359-5.462,13.2,13.2,0,0,1,1.01-1.411.558.558,0,0,1,.324-.227,14.593,14.593,0,0,1,1.5-.1.737.737,0,0,1,.628.387,18.349,18.349,0,0,1,1.687,3.291,1.582,1.582,0,0,1-.213,1.462,10.121,10.121,0,0,1-.792,1,.348.348,0,0,0-.082.388,4.6,4.6,0,0,0,1.026,1.574,16.093,16.093,0,0,0,4.046,3.135c.418.233.495.231.81-.125.456-.517.894-1.05,1.319-1.594a1.051,1.051,0,0,1,1.325-.4,5.282,5.282,0,0,1,.76.414c.857.526,1.708,1.061,2.565,1.589.41.251.458.361.368.837a5.5,5.5,0,0,1-1.037,2.446,4.374,4.374,0,0,1-3.113,1.607c-.355.04-.715.046-1.072.068m1.1-6.063c-.108.148-.178.257-.26.354a15.456,15.456,0,0,1-1.272,1.464c-.826.753-1.118.707-1.9.3a14.844,14.844,0,0,1-4.828-3.884,4.491,4.491,0,0,1-.845-1.592,1,1,0,0,1,.258-1.118,6.156,6.156,0,0,0,.676-.8.881.881,0,0,0,.121-1.022c-.431-.812-.859-1.625-1.266-2.45a.523.523,0,0,0-.59-.308.927.927,0,0,0-.859.475c-.268.493-.6.955-.828,1.464a4.5,4.5,0,0,0-.064,3.723,15.691,15.691,0,0,0,4.311,5.922A9.753,9.753,0,0,0,81.98,353.6a3.806,3.806,0,0,0,2.31-.775,3.67,3.67,0,0,0,1.231-2.21.293.293,0,0,0-.126-.229q-1.2-.767-2.4-1.512c-.17-.106-.354-.2-.542-.3" transform="translate(-69.418 -338.552)"/>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
+            <button class="border rounded-full p-3 bg-[#F7F7F7] drop-shadow-xl">
+                <img src="../../../img/whatsapp.svg" />
             </button>
         </a>
      </div>
      <div class="m-2">
         <a href="mailto: renato.ortiz@coorsamexico.com">
-          <button class="border rounded-full p-3 bg-[#F7F7F7]">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="21.763" viewBox="0 0 28.7 21.763">
-                <defs>
-                 <clipPath id="clip-path">
-                   <rect id="Rectángulo_48" data-name="Rectángulo 48" width="28.7" height="21.763" fill="none" stroke="#f7f7f7" stroke-width="0.5"/>
-                 </clipPath>
-                 <clipPath id="clip-path-2">
-                   <rect id="Rectángulo_49" data-name="Rectángulo 49" width="21.839" height="15.792" fill="none" stroke="#f7f7f7" stroke-width="0.5"/>
-                 </clipPath>
-               </defs>
-               <g id="Grupo_121" data-name="Grupo 121" transform="translate(-12.338 -13.778)">
-                 <g id="Grupo_111" data-name="Grupo 111" transform="translate(12.338 13.778)" opacity="0.35">
-                   <g id="Grupo_110" data-name="Grupo 110">
-                     <g id="Grupo_109" data-name="Grupo 109" clip-path="url(#clip-path)">
-                       <path id="Trazado_31" data-name="Trazado 31" d="M108.918,338.752a3.19,3.19,0,0,1,.574-1.347,2.642,2.642,0,0,1,1.982-1.006c.093-.005.187-.005.28-.005q11.516,0,23.035,0a2.7,2.7,0,0,1,2.292,1.064,2.784,2.784,0,0,1,.537,1.759q0,7.891-.006,15.781a4.878,4.878,0,0,1-.121,1.246,2.567,2.567,0,0,1-2.367,1.9c-.092.007-.185.009-.279.009q-11.576,0-23.149,0a2.6,2.6,0,0,1-2.508-1.512,5.631,5.631,0,0,1-.271-.846Zm14.365-.843h-8.877c-1,0-2.006,0-3.009,0a.932.932,0,0,0-.989.66,1.839,1.839,0,0,0-.044.482q0,8.22,0,16.443c0,.075,0,.152,0,.229a.917.917,0,0,0,.889.907c.1.009.209.007.313.007q11.7,0,23.4,0a3.339,3.339,0,0,0,.429-.02.911.911,0,0,0,.77-.808c.009-.1.009-.209.009-.315V339.054c0-.077,0-.152,0-.229a.912.912,0,0,0-.887-.907c-.114-.009-.229-.009-.343-.009H123.283" transform="translate(-108.918 -336.391)" stroke="#f7f7f7" stroke-width="0.5"/>
-                     </g>
-                   </g>
-                 </g>
-                 <g id="Grupo_114" data-name="Grupo 114" transform="translate(15.765 16.807)" opacity="0.35">
-                   <g id="Grupo_113" data-name="Grupo 113">
-                     <g id="Grupo_112" data-name="Grupo 112" clip-path="url(#clip-path-2)">
-                       <path id="Trazado_32" data-name="Trazado 32" d="M112.475,341.192v12.644h-1.687V338.044c.117.073.216.132.313.2q5.1,3.309,10.191,6.624a.662.662,0,0,0,.834,0q5.087-3.323,10.189-6.625c.093-.061.187-.117.311-.2v15.79h-1.678V341.2c-.132.077-.216.123-.3.174-2.514,1.633-5.033,3.254-7.534,4.9a2.323,2.323,0,0,1-2.809,0c-2.5-1.651-5.022-3.271-7.534-4.9-.077-.051-.158-.1-.3-.181" transform="translate(-110.788 -338.045)" stroke="#f7f7f7" stroke-width="0.5"/>
-                     </g>
-                   </g>
-                 </g>
-               </g>
-            </svg>
+          <button class="border rounded-full p-3 bg-[#F7F7F7] drop-shadow-xl">
+              <img class="" src="../../../img/gmail.svg" />
           </button>
         </a>
      </div>
      <div class="m-2">
        <a href="https://goo.gl/maps/jn9h3GzbkUrTjT168">
-         <button class="border rounded-full p-2 pl-3 pr-3 bg-[#F7F7F7]">
+         <button class="border rounded-full p-2 pl-3 pr-3 bg-[#F7F7F7] drop-shadow-xl">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21.393" height="27.112" viewBox="0 0 21.393 27.112">
                <defs>
                  <clipPath id="clip-path">
