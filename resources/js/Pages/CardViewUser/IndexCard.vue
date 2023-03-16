4fd637200a68cf260f6d134480d302a6b0a9ad07
Hector <programadorjr@coorsamexico.com>
2023-03-16 09:58:14 -0600

    cambios de informacion

--- resources/js/Pages/CardViewUser/IndexCard.vue
+++ resources/js/Pages/CardViewUser/IndexCard.vue
@@ -102,7 +102,27 @@ const share = () =>
            </a>
         </div>
         <div class="m-2">
-          <a href="https://goo.gl/maps/jn9h3GzbkUrTjT168">
+          <a v-if="dato.ubicacion_google" :href=" dato.ubicacion_google">
+            <button class="border rounded-full p-2 pl-3 pr-3 bg-[#F7F7F7] drop-shadow-xl">
+               <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21.393" height="27.112" viewBox="0 0 21.393 27.112">
+                  <defs>
+                    <clipPath id="clip-path">
+                      <rect id="Rectángulo_47" data-name="Rectángulo 47" width="21.393" height="27.112" fill="none"/>
+                    </clipPath>
+                  </defs>
+                  <g id="Grupo_122" data-name="Grupo 122" transform="translate(-15.832 -12.221)">
+                    <g id="Grupo_108" data-name="Grupo 108" transform="translate(15.832 12.221)" opacity="0.35">
+                      <g id="Grupo_107" data-name="Grupo 107">
+                        <g id="Grupo_106" data-name="Grupo 106" clip-path="url(#clip-path)">
+                          <path id="Trazado_30" data-name="Trazado 30" d="M169.951,338.607a10.537,10.537,0,0,0-14.86,0c-3.677,3.679-4.253,8.095-1.711,13.128,2.087,4.128,5.841,7.882,8.584,10.625l.108.108a.637.637,0,0,0,.9,0l.108-.108c2.743-2.743,6.5-6.5,8.584-10.625C174.2,346.7,173.629,342.286,169.951,338.607Zm.577,12.555c-1.911,3.782-5.363,7.311-8.007,9.958-2.644-2.648-6.1-6.177-8.007-9.958-2.3-4.551-1.816-8.364,1.477-11.655a9.245,9.245,0,0,1,13.06,0C172.344,342.8,172.826,346.611,170.528,351.162Zm-8.007-9.17a4.669,4.669,0,1,0,4.669,4.668A4.674,4.674,0,0,0,162.521,341.992Zm0,8.065a3.4,3.4,0,1,1,3.4-3.4,3.4,3.4,0,0,1-3.4,3.4" transform="translate(-151.825 -335.542)"/>
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+            </button>
+          </a>
+          <a v-else href="https://goo.gl/maps/jn9h3GzbkUrTjT168">
             <button class="border rounded-full p-2 pl-3 pr-3 bg-[#F7F7F7] drop-shadow-xl">
                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21.393" height="27.112" viewBox="0 0 21.393 27.112">
                   <defs>
