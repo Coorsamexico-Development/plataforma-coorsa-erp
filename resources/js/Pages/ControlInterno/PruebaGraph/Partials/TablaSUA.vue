41d6ccdb3de8375ed000f81fab6f8b3f520ad01a
Jonathan Hernandez <jhonyhs95@outlook.com>
2023-08-17 16:54:34 -0600

    SUA

--- resources/js/Pages/ControlInterno/PruebaGraph/Partials/TablaSUA.vue
+++ resources/js/Pages/ControlInterno/PruebaGraph/Partials/TablaSUA.vue
@@ -44,7 +44,7 @@ onBeforeUpdate(() => {
         </thead>
         <tbody>
             <tr
-                class="overflow-hidden bg-white dark:bg-gray-800"
+                class="overflow-hidden bg-white dark:bg-gray-800 even:bg-[#3A7D7F]/5"
                 v-for="rubro in auxArray"
                 :key="id"
             >
