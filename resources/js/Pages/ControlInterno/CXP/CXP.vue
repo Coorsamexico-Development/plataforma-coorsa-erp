3dface2039055bbe89b2a3d8596b6137444e9ed4
alan tonatiuh <programador.srcoorsamexico.com>
2024-05-28 09:44:29 -0600

    Nominas CI

---
+++ resources/js/Pages/ControlInterno/CXP/CXP.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { watchEffect, ref, reactive, onMounted } from "vue";
+import CardCi from "../Partials/CardCi.vue";
+
+const props = defineProps({
+    show: {
+        type: Object,
+        default: false,
+    },
+});
+</script>
+<template>
+    <CardCi></CardCi>
+</template>
