c44863f23e0657277c9b4fdc1382e0ca2414389a
Jonathan Hernandez <jhonyhs95@outlook.com>
2024-06-13 09:59:32 -0600

    ChatBot

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -10,14 +10,14 @@ class ChatBotController extends Controller
 {
     public function chatBot(Request $request)
     {
-        $message = json_encode($request);
+        $message = json_encode($request->request);
         event(new ChatBot($message));
         return response($request['hub_challenge'], 200);
     }
 
     public function chatBotData(Request $request)
     {
-        event(new ChatBot(json_encode($request)));
+        event(new ChatBot(json_encode($request->request)));
         return response($request['hub_challenge'], 200);
     }
 }
