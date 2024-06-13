78e1ad2c54c3371b256d59bee40fd4042ba08c18
Jonathan Hernandez <jhonyhs95@outlook.com>
2024-06-13 16:02:34 -0600

    ChatBot

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use App\Events\ChatBot;
+use Exception;
 use Illuminate\Http\Request;
 
 class ChatBotController extends Controller
@@ -22,26 +23,34 @@ class ChatBotController extends Controller
 
     public function chatBotData(Request $request)
     {
-        $message = json_decode($request->getContent(), true);
+        try {
+            $message = json_decode($request->getContent(), true);
 
-        $value = $message['entry'][0]['changes'][0]['value'];
+            $value = $message['entry'][0]['changes'][0]['value'];
 
-        if (!empty($value['messages']))
-            switch ($value['messages'][0]['type']) {
-                case 'text':
-                    $body = $value['messages'][0]['text']['body'];
-                    event(new ChatBot($body));
-                    break;
-                case 'document':
-                    $body = $value['messages'][0]['document'];
-                    event(new ChatBot($body));
-                    break;
-                case 'image':
-                    $body = $value['messages'][0]['image'];
-                    event(new ChatBot($body));
-                    break;
-            }
-
-        return response($request['hub_challenge'], 200);
+            if (!empty($value['messages']))
+                switch ($value['messages'][0]['type']) {
+                    case 'text':
+                        $body = $value['messages'][0]['text']['body'];
+                        event(new ChatBot($body));
+                        break;
+                    case 'document':
+                        $body = $value['messages'][0]['document'];
+                        event(new ChatBot($body));
+                        break;
+                    case 'image':
+                        $body = $value['messages'][0]['image'];
+                        event(new ChatBot($body));
+                        break;
+                }
+            return response()->json([
+                'success' => true,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
