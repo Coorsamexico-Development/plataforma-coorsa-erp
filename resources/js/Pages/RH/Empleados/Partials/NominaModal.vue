aec2d3353320f5b9243e1ed8858fcbed961ad4ba
Jonathan Hernandez <jhonyhs95@outlook.com>
2024-10-08 12:57:34 -0600

    Recibos de Nomina

--- resources/js/Pages/RH/Empleados/Partials/NominaModal.vue
+++ resources/js/Pages/RH/Empleados/Partials/NominaModal.vue
@@ -98,7 +98,7 @@ const subir = () => {
                     type="submit"
                     value="Subir"
                     class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition bg-blue-600 border border-transparent rounded-md hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-300 disabled:opacity-25 hover:cursor-pointer"
-                    :disabled="form.progress || form.doc === ''"
+                    :disabled="form.processing || form.doc === ''"
                 />
             </form>
         </template>
